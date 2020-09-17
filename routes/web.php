ec774ba2a237ca476da41e8ea7bd120e1d46531e
Sirichai-a <sirichai868@gmail.com>
2020-09-17 22:12:10 +0700

    Revert "update route"
    This reverts commit b3eb179e2dc726bcddafe8db9bcb85729200fbab.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,5 +20,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get();
